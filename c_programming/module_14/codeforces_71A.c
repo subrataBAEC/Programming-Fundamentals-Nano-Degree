ed870c819cb82445e4a12622fe2fd335efb309c4
Subrata Saha <72705320+subrataBAEC@users.noreply.github.com>
2022-05-11 15:57:34 +0600

    Add files via upload

---
+++ c_programming/module_14/codeforces_71A.c
@@ -0,0 +1,29 @@
+//codeforces Way Too Long Words
+//input: localization
+//output: l10n
+/* logic:
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i=0; i<n; i++){
+            char word[101];
+            scanf("%s",word);
+            int len=strlen(word);
+            if(len<=10){
+                printf("%s\n",word);
+            }
+            else{
+                printf("%c%d%c\n",word[0],len-2,word[len-1]);
+            }
+    }
+
+    return 0;
+}
