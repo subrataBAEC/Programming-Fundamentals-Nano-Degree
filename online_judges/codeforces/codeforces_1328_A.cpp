45e9b582f094cd227b8bb089dd8b005d14dd26e8
Subrata Saha <72705320+subrataBAEC@users.noreply.github.com>
2022-07-12 23:58:14 +0600

    Add files via upload

--- online_judges/codeforces/codeforces_1328_A.cpp
+++ online_judges/codeforces/codeforces_1328_A.cpp
@@ -1,14 +1,26 @@
 //codeforces 1328/A complete
 /* 
-
+using two for loops causes time limit problem
 */
 #include<bits/stdc++.h> // all in one
 using namespace std;
 int main() {
     long long int t,a,b;
     cin>>t;   
-
-    int res=0;
+    // this creates time limit problem
+    // for(int i=0; i<t; i++){
+    //     long long int count=0;
+    //     cin>>a>>b;
+    //     for(int i=0; ;i++){
+    //        if((a%b)==0){
+    //         break;
+    //        }
+    //        count++;
+    //        a++; 
+    //     }
+    //     cout<<count<<endl;    
+    // }
+    int res;
     for(int i=0; i<t; i++){
         cin>>a>>b;
         if(a%b==0) cout<<0<<endl;
