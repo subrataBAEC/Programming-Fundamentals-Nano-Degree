639eeef6d197931735efbb2cc9b6761f1013ef3a
Subrata Saha <72705320+subrataBAEC@users.noreply.github.com>
2022-07-22 23:57:49 +0600

    Add files via upload

--- Data_structure/8_module_Exam 01/exam_01_final.cpp
+++ Data_structure/8_module_Exam 01/exam_01_final.cpp
@@ -1,42 +1,55 @@
-//exam 01- incomplete
+//exam 01- complete
 /* 
-restaurant billing system--complete except question 7
+restaurant billing system--complete
 */
 #include<bits/stdc++.h> // all in one
 using namespace std;
 #define width 25
-#define size 5
+#define sz 12 //array size
 
 class Restaurant{
 public:
-    int food_item_codes[5];
-    string food_item_names[5];
-    int food_item_prices[5];
+    int food_item_codes[sz];
+    string food_item_names[sz];
+    int food_item_prices[sz];
     int total_tax;
 
     //constructor-store values
     Restaurant(int codes[], string names[], int prices[]){
         //int sum=0;
         //cout<<"hi"<<endl;
-        for(int i=0; i<5; i++){            
+        for(int i=0; i<sz; i++){            
             food_item_codes[i]=codes[i];
             food_item_prices[i]=prices[i];
             food_item_names[i]=names[i];            
         }
     }
+    //set tax value
     void set_tax(int tax){
         total_tax=tax;
         cout<<endl;
         cout<<"Tax received in Restaurant class: "<<total_tax<<endl;
     }
+    //print food menue
+    void show_menue(){
+        cout<<"                     MAKE BILL"<<endl;
+        cout<<"----------------------------------------------------"<<endl;
+        cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<endl;
+        cout<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<endl;
+        
+        for(int j=0; j<sz; j++){
+            cout<<setw(width)<<left<<food_item_codes[j]<<setw(width)<<left<<food_item_names[j]<<setw(width)<<left<<food_item_prices[j]<<endl;        
+        }
+        cout<<endl;
+    }
 };
 
 //function to store values
 Restaurant* storeValues(){
-    int codes[5],prices[5];
-    string names[5];
+    int codes[sz],prices[sz];
+    string names[sz];
 
-    for(int i=0; i<5; i++){
+    for(int i=0; i<sz; i++){
         cin>>codes[i];
         //cin>>names[i];// doesnot work with space
         getline(cin>>ws,names[i]);//string with space
@@ -48,24 +61,15 @@ Restaurant* storeValues(){
 }
 
 int main(){   
-    cout<<"enter 5 values "<<width<<endl;
-    Restaurant *restaurant; 
+    cout<<"Restaurant bill system: "<<sz<<"food Items"<<endl;
 
+    Restaurant *restaurant; 
     //input    
     restaurant=storeValues();
 
     //output--offered food menue
-    cout<<"                     MAKE BILL"<<endl;
-    cout<<"----------------------------------------------------"<<endl;
-    cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<endl;
-    cout<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<endl;
-    
-    for(int j=0; j<5; j++){
-        cout<<setw(width)<<left<<restaurant-> food_item_codes[j]<<setw(width)<<left<<restaurant->food_item_names[j]<<setw(width)<<left<<restaurant->food_item_prices[j]<<endl;        
-    }
-    cout<<endl;
-    cout<<endl;
-
+    restaurant->show_menue();
+  
     //table information- customer order
     int table,n;
     cout<<"Enter Table No : ";   
@@ -78,7 +82,7 @@ int main(){
         cout<<"Enter Item No "<<i+1<<" Code : ";
         cin>>items[i];
         //check if item code is valid
-        for(int j=0; j<5; j++){
+        for(int j=0; j<sz; j++){
             if(restaurant->food_item_codes[j]==items[i]){
                flag=1; 
                break;               
@@ -101,7 +105,7 @@ int main(){
     cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<setw(width)<<left<<"Item Quantity"<<setw(width)<<left<<"Total Price"<<endl;
     int grand_total=0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<5; j++){
+        for(int j=0; j<sz; j++){
             if(restaurant->food_item_codes[j]==items[i]){
                 //cout<<"found ";
                 int item_total_price=0;
--- Data_structure/8_module_Exam 01/exam_01_final_modified.cpp
+++ Data_structure/8_module_Exam 01/exam_01_final_modified.cpp
@@ -1,42 +1,55 @@
-//exam 01- incomplete
+//exam 01- complete
 /* 
-restaurant billing system--complete except question 7
+restaurant billing system--complete
 */
 #include<bits/stdc++.h> // all in one
 using namespace std;
 #define width 25
-#define size 5
+#define sz 12 //array size
 
 class Restaurant{
 public:
-    int food_item_codes[5];
-    string food_item_names[5];
-    int food_item_prices[5];
+    int food_item_codes[sz];
+    string food_item_names[sz];
+    int food_item_prices[sz];
     int total_tax;
 
     //constructor-store values
     Restaurant(int codes[], string names[], int prices[]){
         //int sum=0;
         //cout<<"hi"<<endl;
-        for(int i=0; i<5; i++){            
+        for(int i=0; i<sz; i++){            
             food_item_codes[i]=codes[i];
             food_item_prices[i]=prices[i];
             food_item_names[i]=names[i];            
         }
     }
+    //set tax value
     void set_tax(int tax){
         total_tax=tax;
         cout<<endl;
         cout<<"Tax received in Restaurant class: "<<total_tax<<endl;
     }
+    //print food menue
+    void show_menue(){
+        cout<<"                     MAKE BILL"<<endl;
+        cout<<"----------------------------------------------------"<<endl;
+        cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<endl;
+        cout<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<endl;
+        
+        for(int j=0; j<sz; j++){
+            cout<<setw(width)<<left<<food_item_codes[j]<<setw(width)<<left<<food_item_names[j]<<setw(width)<<left<<food_item_prices[j]<<endl;        
+        }
+        cout<<endl;
+    }
 };
 
 //function to store values
 Restaurant* storeValues(){
-    int codes[5],prices[5];
-    string names[5];
+    int codes[sz],prices[sz];
+    string names[sz];
 
-    for(int i=0; i<5; i++){
+    for(int i=0; i<sz; i++){
         cin>>codes[i];
         //cin>>names[i];// doesnot work with space
         getline(cin>>ws,names[i]);//string with space
@@ -48,24 +61,15 @@ Restaurant* storeValues(){
 }
 
 int main(){   
-    cout<<"enter 5 values "<<width<<endl;
+    cout<<"Restaurant bill system: "<<sz<<" food Items"<<endl;
+    //object
     Restaurant *restaurant; 
-
     //input    
     restaurant=storeValues();
 
     //output--offered food menue
-    cout<<"                     MAKE BILL"<<endl;
-    cout<<"----------------------------------------------------"<<endl;
-    cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<endl;
-    cout<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<setw(width)<<left<<"------"<<endl;
-    
-    for(int j=0; j<5; j++){
-        cout<<setw(width)<<left<<restaurant-> food_item_codes[j]<<setw(width)<<left<<restaurant->food_item_names[j]<<setw(width)<<left<<restaurant->food_item_prices[j]<<endl;        
-    }
-    cout<<endl;
-    cout<<endl;
-
+    restaurant->show_menue();
+  
     //table information- customer order
     int table,n;
     cout<<"Enter Table No : ";   
@@ -78,14 +82,15 @@ int main(){
         cout<<"Enter Item No "<<i+1<<" Code : ";
         cin>>items[i];
         //check if item code is valid
-        for(int j=0; j<5; j++){
+        for(int j=0; j<sz; j++){
             if(restaurant->food_item_codes[j]==items[i]){
                flag=1; 
                break;               
             }
         }
         if(flag==0){
-        cout<<"not found,invalid "<<endl;;
+        cout<<"Code Not found,invalid,Start Again "<<endl;
+        return 0;//end everything
         break;
         }
         cout<<"Enter Item No "<<i+1<<" Quantity : ";
@@ -101,7 +106,7 @@ int main(){
     cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<setw(width)<<left<<"Item Quantity"<<setw(width)<<left<<"Total Price"<<endl;
     int grand_total=0;
     for(int i=0; i<n; i++){
-        for(int j=0; j<5; j++){
+        for(int j=0; j<sz; j++){
             if(restaurant->food_item_codes[j]==items[i]){
                 //cout<<"found ";
                 int item_total_price=0;
@@ -120,7 +125,7 @@ int main(){
     
     // set tax to class member--total_tax
     restaurant->set_tax(tax);
-    cout<<restaurant->total_tax<<endl;
+    cout<<"output from restaurant class "<<restaurant->total_tax<<endl;
     return 0;
 }
 /*
@@ -160,6 +165,4 @@ Chicken Sizzling 1:3
 453
 Beef Sizzling 1:3
 970
-
-
 */
