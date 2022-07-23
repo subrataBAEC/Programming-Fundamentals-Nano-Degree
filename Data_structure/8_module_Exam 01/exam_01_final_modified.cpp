9c31d391c3e3f9c134b4b1e789f2e0d7df9de796
Subrata Saha <72705320+subrataBAEC@users.noreply.github.com>
2022-07-23 19:27:49 +0600

    Add files via upload

--- Data_structure/8_module_Exam 01/exam_01_final_modified.cpp
+++ Data_structure/8_module_Exam 01/exam_01_final_modified.cpp
@@ -12,7 +12,7 @@ public:
     int food_item_codes[sz];
     string food_item_names[sz];
     int food_item_prices[sz];
-    int total_tax;
+    int total_tax=0;
 
     //constructor-store values
     Restaurant(int codes[], string names[], int prices[]){
@@ -21,14 +21,15 @@ public:
         for(int i=0; i<sz; i++){            
             food_item_codes[i]=codes[i];
             food_item_prices[i]=prices[i];
-            food_item_names[i]=names[i];            
+            food_item_names[i]=names[i];
+            //total_tax=0;   //to set default value to zero         
         }
     }
     //set tax value
     void set_tax(int tax){
-        total_tax=tax;
+        total_tax=total_tax+tax;
         cout<<endl;
-        cout<<"Tax received in Restaurant class: "<<total_tax<<endl;
+        cout<<"Tax received in Restaurant class:(including all previous taxes)  "<<total_tax<<endl;
     }
     //print food menue
     void show_menue(){
@@ -67,67 +68,85 @@ int main(){
     //input    
     restaurant=storeValues();
 
-    //output--offered food menue
-    restaurant->show_menue();
-  
-    //table information- customer order
-    int table,n;
-    cout<<"Enter Table No : ";   
-    cin>>table;
-    cout<<"Enter Number of Items : ";
-    cin>>n;
-    int items[n],quantity[n];
-    for(int i=0; i<n; i++){
-        int flag=0;
-        cout<<"Enter Item No "<<i+1<<" Code : ";
-        cin>>items[i];
-        //check if item code is valid
-        for(int j=0; j<sz; j++){
-            if(restaurant->food_item_codes[j]==items[i]){
-               flag=1; 
-               break;               
-            }
-        }
-        if(flag==0){
-        cout<<"Code Not found,invalid,Start Again "<<endl;
-        return 0;//end everything
-        break;
-        }
-        cout<<"Enter Item No "<<i+1<<" Quantity : ";
-        cin>>quantity[i];
-    }
-    cout<<endl;
-    cout<<endl;
-
-    //show bill summary
-    cout<<"                                     BILL SUMMARY"<<endl;
-    cout<<"----------------------------------------------------------------------------------------------------------------"<<endl;
-    cout<<"Table No: "<<table<<endl; 
-    cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<setw(width)<<left<<"Item Quantity"<<setw(width)<<left<<"Total Price"<<endl;
-    int grand_total=0;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<sz; j++){
-            if(restaurant->food_item_codes[j]==items[i]){
-                //cout<<"found ";
-                int item_total_price=0;
-                item_total_price=quantity[i]*restaurant->food_item_prices[j];
-                grand_total=grand_total+item_total_price;
-                cout<<setw(width)<<left<<restaurant-> food_item_codes[j]<<setw(width)<<left<<restaurant->food_item_names[j]<<setw(width)<<left<<restaurant->food_item_prices[j]<<setw(width)<<left<<quantity[i]<<setw(width)<<left<<item_total_price<<endl;
-            }
-        }
+    //keep whole thing in an infinite loop
+    for(int k=0; ; k++){
+        start:
+            //output--offered food menue
+            restaurant->show_menue();
         
-    }    
-    double tax=grand_total*0.05; 
-    cout<<"TAX"<<setw(97)<<left<<" "<<tax<<endl;
-    cout<<"----------------------------------------------------------------------------------------------------------------"<<endl;
-    grand_total=ceil(grand_total+tax);
-    cout<<"NET TOTAL"<<setw(91)<<left<<" "<<ceil(grand_total)<<" Taka(Rounded Value)"<<endl;
-    
-    // set tax to class member--total_tax
-    restaurant->set_tax(tax);
-    cout<<"output from restaurant class "<<restaurant->total_tax<<endl;
-    return 0;
+            //table information- customer order
+            int table,n;
+            cout<<"Enter Table No : ";   
+            cin>>table;
+            cout<<"Enter Number of Items : ";
+            cin>>n;
+            int items[n],quantity[n];
+            for(int i=0; i<n; i++){
+                int flag=0;
+                cout<<"Enter Item No "<<i+1<<" Code : ";
+                cin>>items[i];
+                //check if item code is valid
+                for(int j=0; j<sz; j++){
+                    if(restaurant->food_item_codes[j]==items[i]){
+                    flag=1; 
+                    break;               
+                    }
+                }
+                if(flag==0){
+                cout<<"Code Not found,invalid,Start Again "<<endl;
+                //return 0;//end everything
+                //continue;
+                goto start;
+                //break;
+                }
+                cout<<"Enter Item No "<<i+1<<" Quantity : ";
+                cin>>quantity[i];
+            }
+            cout<<endl;
+            cout<<endl;
+
+            //show bill summary
+            cout<<"                                     BILL SUMMARY"<<endl;
+            cout<<"----------------------------------------------------------------------------------------------------------------"<<endl;
+            cout<<"Table No: "<<table<<endl; 
+            cout<<setw(width)<<left<<"Item code"<<setw(width)<<left<<"Item name"<<setw(width)<<left<<"Item price"<<setw(width)<<left<<"Item Quantity"<<setw(width)<<left<<"Total Price"<<endl;
+            int grand_total=0;
+            for(int i=0; i<n; i++){
+                for(int j=0; j<sz; j++){
+                    if(restaurant->food_item_codes[j]==items[i]){
+                        //cout<<"found ";
+                        int item_total_price=0;
+                        item_total_price=quantity[i]*restaurant->food_item_prices[j];
+                        grand_total=grand_total+item_total_price;
+                        cout<<setw(width)<<left<<restaurant-> food_item_codes[j]<<setw(width)<<left<<restaurant->food_item_names[j]<<setw(width)<<left<<restaurant->food_item_prices[j]<<setw(width)<<left<<quantity[i]<<setw(width)<<left<<item_total_price<<endl;
+                    }
+                }
+                
+            }    
+            double tax=grand_total*0.05; 
+            cout<<"TAX"<<setw(97)<<left<<" "<<tax<<endl;
+            cout<<"----------------------------------------------------------------------------------------------------------------"<<endl;
+            grand_total=ceil(grand_total+tax);
+            cout<<"NET TOTAL"<<setw(91)<<left<<" "<<ceil(grand_total)<<" Taka(Rounded Value)"<<endl;
+            cout<<"tax in this order "<<tax<<endl;
+            
+            // set tax to class member--total_tax
+            restaurant->set_tax(tax);
+            
+            cout<<"output from restaurant class "<<restaurant->total_tax<<endl;
+            cout<<endl;
+            //press 1 to continue, 0 to end
+            cout<<"want to place another order? enter 1. if not enter 0"<<endl;
+            int quit=1;
+            cin>>quit;
+            if(quit==0) return 0;
+            else continue;
+        }
+        return 0;
+
 }
+
+    
 /*
 170
 Chicken Soup 1:2
