65254163ebe4fbadef53699159e17fcfdcff6d0c
Subrata Saha <72705320+subrataBAEC@users.noreply.github.com>
2022-12-03 01:09:12 +0600

    Add files via upload

---
+++ OOP_with_python/35_module_Theory_FInal/7.py
@@ -0,0 +1,12 @@
+# Theory Final Exam
+# module 35 Question 7(complete)
+
+"""
+o/p: [3, 6, 9, 12, 15, 18]
+"""
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
+
+new=lst[2::3]
+print(new)
